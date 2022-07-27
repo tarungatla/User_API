5cd13df2564f875d0651d3cdc38385f4d0db6dce
Tarunkumar <tarungatla75@gmail.com>
2022-07-27 13:50:47 +0530

    Added the functionality to change the time when user account is created,updated,accessed

--- models.py
+++ models.py
@@ -9,12 +9,24 @@ class User(models.Model):
     first_name = fields.CharField(max_length=250)
     last_name =  fields.CharField(max_length=250)
     middle_name = fields.CharField(max_length=250)
-    created = fields.CharField(max_length=250)
-    updated = fields.CharField(max_length=250)
-    accessed = fields.CharField(max_length=250)
+    created = fields.IntField()
+    updated = fields.IntField()
+    accessed = fields.IntField()
     status = fields.IntField()
+    
+
+class User_Details(models.Model):
+    user_id = fields.IntField(pk=True)
+    email = fields.CharField(max_length=250)
+    phone = fields.CharField(max_length=250)
+    password = fields.CharField(max_length=250)
+    first_name = fields.CharField(max_length=250)
+    last_name =  fields.CharField(max_length=250)
+    middle_name = fields.CharField(max_length=250)
+
     
 
 User_Pydantic = pydantic_model_creator(User, name="User") #pydantic model for tortoise
 UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly=True)  #does not take read only values
+UserDetails_Pydantic = pydantic_model_creator(User_Details, name="User_Details", exclude_readonly=True)  #does not take read only values
 
